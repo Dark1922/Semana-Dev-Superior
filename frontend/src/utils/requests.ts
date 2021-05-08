280839dbbb29cac284250f9fa1c4ebdf31fd08ca
dark1922 <jvdematos004@gmail.com>
2021-05-08 20:09:40 -0300

    envio pro netfly

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,3 +1,4 @@
 
 //nossa api banco de dados
-export const BASE_URL = 'https://sds3-dev-spring-react.herokuapp.com'
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://sds3-dev-spring-react.herokuapp.com'
+//significa que a baseurl da sua aplicação vai ser pra pegar a variavel que botamos no nestfly esse react_app e de lá se estiver definidada ele vai epgar o backend se n tiver definida ela vai pegar o do lado da ?? 
